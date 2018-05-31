2d5bec46e319618c30cec2b6e917390420df6318
John Fraizer <jfraizer@cumulusnetworks.com>
2018-05-31 20:42:31 +0000

    More updates

---
+++ test/configs/get-configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in $1*
+do
+echo -e "\n$i\n============"
+ssh $i "cd /; sudo tar -zcf ~/configs.tgz /etc/frr/frr.conf /etc/frr/daemons /etc/network/interfaces; sudo chown cumulus ~/configs.tgz"
+cd $i
+scp $i:configs.tgz .
+tar xf configs.tgz
+cd ..
+echo
+done
+
