64bbdfd611d251035c6df7d776c23c481b20c4fe
John Fraizer <jfraizer@cumulusnetworks.com>
2018-06-01 05:22:54 +0000

    Fix resolv.conf on devices so BGP default pointing to H3C devices doesn't cause slow SSH connections.
    Note: /etc/resolv.conf has been changed to immutable.  Change back with sudo chattr -i /etc/resolv.conf

--- test/configs/get-configs.sh
+++ test/configs/get-configs.sh
@@ -3,7 +3,7 @@
 for i in $1*
 do
 echo -e "\n$i\n============"
-ssh $i "cd /; sudo tar -zcf ~/configs.tgz /etc/frr/frr.conf /etc/frr/daemons /etc/network/interfaces; sudo chown cumulus ~/configs.tgz"
+ssh $i "cd /; sudo tar -zcf ~/configs.tgz /etc/frr/frr.conf /etc/frr/daemons /etc/network/interfaces /etc/resolv.conf; sudo chown cumulus ~/configs.tgz"
 cd $i
 scp $i:configs.tgz .
 tar xf configs.tgz
